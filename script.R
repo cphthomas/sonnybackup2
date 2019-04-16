b748760748f371cc776b3eeb2950e2a2bf1f1e38
cphthomas <cphthomas\@gmail.com>
2019-04-16 00:22:37 +0000

    .

--- script.R
+++ script.R
@@ -1,3 +1,3 @@
-# rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
+rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
 system("git config --global user.email 'cphthomas\\@gmail.com' && git config --global user.name 'cphthomas' && git config --global credential.helper 'cache --timeout 300'")
 system("git add . && git commit -a -m . && git push")
