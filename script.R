8810f2ebd7074264507de29c36e813699cd8e3c5
cphthomas <cphthomas@gmail.com>
2019-04-16 00:08:32 +0000

    .

--- script.R
+++ script.R
@@ -1,3 +1,2 @@
-# rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
-# git add . && git commit -a -m . && git push
+rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
 system("git add . && git commit -a -m . && git push")
